8a92c29620bf6b2da6054b8249498b9210a83e88
yukikom-biz <yuki.komatsu@bizreach.co.jp>
2019-04-26 17:21:55 +0900

    new page

---
+++ script.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+    console.log("road is ok");
+}
+
+
+function changeImage() {
+    var topimage = document.getElementById('droid-img');
+    topimage.style.backgroundImage("../image/");
+    //todo
+}
